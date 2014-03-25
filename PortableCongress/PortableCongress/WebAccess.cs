81d4075912eadd4aaa2035fb08246daac0eda5ea
Mike Bluestein <mike.bluestein@gmail.com>
2014-03-25 09:18:41 -0400

    Handle exceptions fetching data in web access

--- PortableCongress/PortableCongress/WebAccess.cs
+++ PortableCongress/PortableCongress/WebAccess.cs
@@ -8,67 +8,82 @@ using System.Collections.Generic;
 
 namespace PortableCongress
 {
-	public class WebAccess
-	{
+    public class WebAccess
+    {
         const string API_KEY = "609b2b4b92f74ce9ac5c86487146d107";
 
-		public WebAccess ()
-		{
-		}
+        public WebAccess ()
+        {
+        }
 
-		public static async Task<RecentVotes> GetRecentVotesAsync(int id)
-		{
-			using (var httpClient = new HttpClient())
-			{
-				string url = String.Format ("http://www.govtrack.us/users/events-rss2.xpd?monitors=pv:{0}&days=30", id);
+        public static async Task<RecentVotes> GetRecentVotesAsync (int id)
+        {
+            try {
+                using (var httpClient = new HttpClient ()) {
+                    string url = String.Format ("http://www.govtrack.us/users/events-rss2.xpd?monitors=pv:{0}&days=30", id);
 
-				var response = await httpClient.GetAsync(url);
-				var stream  = await response.Content.ReadAsStreamAsync();
-				var votes = LoadVotes (stream);
-				var recentVotes = new RecentVotes {Id = id, Votes = votes};
+                    var response = await httpClient.GetAsync (url);
+                    var stream = await response.Content.ReadAsStreamAsync ();
+                    var votes = LoadVotes (stream);
+                    var recentVotes = new RecentVotes { Id = id, Votes = votes };
 
-				return recentVotes;
-			}
-		}
+                    return recentVotes;
+                }
+            } catch (Exception) {
+                var recentVotes = new RecentVotes {Id = id, Votes = new List<Vote> {
+                        new Vote { Title = "Could not connect to the internet" }
+                    }
+                };
+                return recentVotes;
+            }
+        }
 
-		public static async Task<Committees> GetCommitteesAsync(int id, string bioGuideId)
-		{
-			using (var httpClient = new HttpClient ()) {
-                string url = String.Format ("http://services.sunlightlabs.com/api/committees.allForLegislator.xml?apikey={0}&bioguide_id={1}", API_KEY, bioGuideId);
+        public static async Task<Committees> GetCommitteesAsync (int id, string bioGuideId)
+        {
+            try {
+                using (var httpClient = new HttpClient ()) {
+                    string url = String.Format ("http://services.sunlightlabs.com/api/committees.allForLegislator.xml?apikey={0}&bioguide_id={1}", API_KEY, bioGuideId);
 
-				var response = await httpClient.GetAsync (url);
-				var stream = await response.Content.ReadAsStreamAsync ();
-				var committeeList = LoadCommittees (stream);
-				var committees = new Committees { Id = id, CommitteeList = committeeList };
+                    var response = await httpClient.GetAsync (url);
+                    var stream = await response.Content.ReadAsStreamAsync ();
+                    var committeeList = LoadCommittees (stream);
+                    var committees = new Committees { Id = id, CommitteeList = committeeList };
 
-				return committees;	
-			}
-		}
+                    return committees;	
+                }
+            } catch (Exception) {
+                var commitees = new Committees {Id = id, CommitteeList = new List<Committee> {
+                        new Committee { Name = "Could not connect to the internet" }
+                    }
+                };
+                return commitees;
+            }
+        }
 
-		static List<Committee> LoadCommittees (Stream stream)
-		{
-			XDocument committeeData = XDocument.Load (stream);
+        static List<Committee> LoadCommittees (Stream stream)
+        {
+            XDocument committeeData = XDocument.Load (stream);
 
-			var committeeList = (from c in committeeData.Descendants ("committee")
-				select new Committee { Name = c.Element ("name").Value }).ToList ();
+            var committeeList = (from c in committeeData.Descendants ("committee")
+                                 select new Committee { Name = c.Element ("name").Value }).ToList ();
 
-			return committeeList;
-		}
+            return committeeList;
+        }
 
-		static List<Vote> LoadVotes (Stream stream)
-		{
-			XDocument voteFeed = XDocument.Load (stream);
+        static List<Vote> LoadVotes (Stream stream)
+        {
+            XDocument voteFeed = XDocument.Load (stream);
 
-			var votes = (from item in voteFeed.Descendants ("item")
-				select new Vote {
-					Title = item.Element ("title").Value,
-					PublicationDate = DateTime.Parse (item.Element ("pubDate").Value),
-					Link = item.Element ("link").Value,
-					Description = item.Element ("description").Value
-				}).OrderByDescending (v =>  v.PublicationDate).ToList();
+            var votes = (from item in voteFeed.Descendants ("item")
+                         select new Vote {
+                            Title = item.Element ("title").Value,
+                            PublicationDate = DateTime.Parse (item.Element ("pubDate").Value),
+                            Link = item.Element ("link").Value,
+                            Description = item.Element ("description").Value
+                        }).OrderByDescending (v => v.PublicationDate).ToList ();
 
-			return votes;
-		}
-	}
+            return votes;
+        }
+    }
 }
 
