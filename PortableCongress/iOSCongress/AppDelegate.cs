fea6cf3cd6cd1d7a1a5c7b09f412b087ddbce867
Joseph Hill <joseph@xamarin.com>
2014-12-27 00:45:39 -0500

    Migrating iOS projects to Unified API

--- PortableCongress/iOSCongress/AppDelegate.cs
+++ PortableCongress/iOSCongress/AppDelegate.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
+using Foundation;
+using UIKit;
 
 namespace iOSCongress
 {
--- PortableCongress/iOSCongress/CongressViewController.cs
+++ PortableCongress/iOSCongress/CongressViewController.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Drawing;
+using CoreGraphics;
 using System.Reflection;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
+using Foundation;
+using UIKit;
 using Congress;
 using PortableCongress;
 
